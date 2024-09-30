cf7d8c78bed624f06931d2eefa802ac8d66a7f82
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2024-09-30 16:14:08 +0200

    add-vpc-ec2

---
+++ infra/vpc.tf
@@ -0,0 +1,73 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "vpc_dataexfiltration" {
+  source                       = "terraform-aws-modules/vpc/aws"
+  version                      = "5.1.2"
+  name                         = var.prefix
+  cidr                         = "10.0.0.0/24"
+  azs                          = data.aws_availability_zones.available.names
+  private_subnets              = ["10.0.0.0/26", "10.0.0.64/26"]
+  private_subnet_names         = ["${var.prefix}_private_1", "${var.prefix}_private_2"]
+  public_subnets               = ["10.0.0.128/26", "10.0.0.192/26"]
+  public_subnet_names          = ["${var.prefix}_public_1", "${var.prefix}_public_2"]
+  public_dedicated_network_acl = true
+  enable_nat_gateway           = true
+  single_nat_gateway           = true
+  enable_dns_hostnames         = true
+  enable_dns_support           = true
+}
+
+module "vpc_endpoints_dataexfiltration" {
+  source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+  version = "~> 5.0"
+
+  vpc_id = module.vpc_dataexfiltration.vpc_id
+
+  endpoints = { for service in toset(["ssm", "ssmmessages", "ec2messages"]) :
+    replace(service, ".", "_") =>
+    {
+      service             = service
+      subnet_ids          = module.vpc_dataexfiltration.private_subnets
+      private_dns_enabled = true
+    }
+  }
+
+  create_security_group      = true
+  security_group_name_prefix = "vpc-endpoints-${var.prefix}"
+  security_group_description = "VPC endpoint security group"
+  security_group_rules = {
+    ingress_https = {
+      description = "HTTPS from subnets"
+      cidr_blocks = module.vpc_dataexfiltration.private_subnets_cidr_blocks
+    }
+  }
+}
+
+module "vpc_endpoints" {
+  source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+  version = "5.13.0"
+
+  vpc_id = module.vpc_dataexfiltration.vpc_id
+
+  create_security_group      = true
+  security_group_name_prefix = "${var.prefix}-vpc-endpoints-"
+  security_group_description = "VPC endpoint security group"
+  security_group_rules = {
+    ingress_https = {
+      description = "HTTPS from VPC"
+      cidr_blocks = [module.vpc_dataexfiltration.vpc_cidr_block]
+    }
+  }
+
+  endpoints = {
+    s3 = {
+      service             = "s3"
+      private_dns_enabled = true
+      dns_options = {
+        private_dns_only_for_inbound_resolver_endpoint = false
+      }
+    }
+  }
+}
