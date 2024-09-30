9237c014c820b43fb787ca73dcc3b001ccbea5e5
GNuccio96 <giuseppenuccio.crea@pagopa.it>
2024-09-30 15:33:16 +0200

    initial commit (#2)
    Co-authored-by: Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>

---
+++ infra/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = "1.9.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.65.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "terraform-backend-20230207141844477000000001"
+    key            = "dataexfiltration/main/tfstate"
+    region         = "eu-south-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  default_tags {
+    tags = var.tags
+  }
+}
+
+resource "random_id" "unique" {
+  byte_length = 3
+}
+
+locals {
+  project = "${var.prefix}-${random_id.unique.hex}"
+}
