6add94d212687a8f7235a2669415029d6da674cb
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2024-10-01 15:07:55 +0200

    add-net-firewall

---
+++ infra/network-firewall.tf
@@ -0,0 +1,77 @@
+data "aws_caller_identity" "current" {}
+#data "aws_availability_zones" "available" {}
+
+locals {
+  region     = "eu-south-1"
+  account_id = data.aws_caller_identity.current.account_id
+  vpc_cidr   = "10.0.0.0/16"
+  num_azs    = 3
+  azs        = slice(data.aws_availability_zones.available.names, 0, local.num_azs)
+
+  subnet_mapping_public = {
+    for i in range(0, local.num_azs) :
+    "subnet-public-${i}" => {
+      subnet_id       = element(module.vpc_dataexfiltration.public_subnets, i)
+      ip_address_type = "IPV4"
+    }
+  }
+
+  subnet_mapping_private = {
+    for i in range(0, local.num_azs) :
+    "subnet-private-${i}" => {
+      subnet_id       = element(module.vpc_dataexfiltration.private_subnets, i)
+      ip_address_type = "IPV4"
+    }
+  }
+  subnet = merge(local.subnet_mapping_public, local.subnet_mapping_private)
+
+}
+
+module "network_firewall_dataexfiltration" {
+  source  = "terraform-aws-modules/network-firewall/aws"
+  version = "1.0.1"
+
+  # Firewall
+  name        = "${local.project}-firewall"
+  description = "Network firewall"
+
+  delete_protection                 = false
+  firewall_policy_change_protection = false
+  subnet_change_protection          = false
+
+  vpc_id         = module.vpc_dataexfiltration.vpc_id
+  subnet_mapping = local.subnet
+
+  # Policy
+  policy_name        = "${local.project}-firewall-policy"
+  policy_description = "Network firewall policy"
+
+  policy_stateful_rule_group_reference = {
+    one = { resource_arn = module.network_firewall_rule_group_stateful_dataexfiltration.arn }
+  }
+}
+
+module "network_firewall_rule_group_stateful_dataexfiltration" {
+  source  = "terraform-aws-modules/network-firewall/aws//modules/rule-group"
+  version = "1.0.1"
+
+  name        = "${local.project}-rule-stateful"
+  description = "Stateful Inspection for denying access to a domain"
+  type        = "STATEFUL"
+  capacity    = 100
+
+  rule_group = {
+    rules_source = {
+      rules_source_list = {
+        generated_rules_type = "ALLOWLIST"
+        target_types         = ["HTTP_HOST"]
+        targets              = [".pagopa.it,pagopa.it"]
+      }
+    }
+  }
+
+  # Resource Policy
+  create_resource_policy     = true
+  attach_resource_policy     = true
+  resource_policy_principals = ["arn:aws:iam::${local.account_id}:root"]
+}
