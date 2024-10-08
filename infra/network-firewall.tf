c347cad5ef56ac1f95f63b404e0037a4ec9c2c3a
pasqualedevita <>
2024-10-08 13:02:08 +0200

    issue

--- infra/network-firewall.tf
+++ infra/network-firewall.tf
@@ -67,62 +67,34 @@ module "network_firewall_rule_group_stateful_dataexfiltration" {
   resource_policy_principals = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
 }
 
-resource "aws_cloudwatch_log_group" "anfw_alert_log_group" {
-  name = "/aws/network-firewall/alert"
+resource "aws_cloudwatch_log_group" "network_firewall_log_group" {
+  name = "/aws/${local.project}-network-firewall"
 }
 
-resource "random_string" "bucket_random_id" {
-  length  = 5
-  special = false
-  upper   = false
-}
-
-resource "aws_s3_bucket" "anfw_flow_bucket" {
-  bucket        = "network-firewall-flow-bucket-${random_string.bucket_random_id.id}"
-  force_destroy = true
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-  bucket = aws_s3_bucket.anfw_flow_bucket.id
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "aws:kms"
-    }
-  }
-}
-
-resource "aws_s3_bucket_ownership_controls" "anfw_flow_bucket_ownership_control" {
-  bucket = aws_s3_bucket.anfw_flow_bucket.id
-  rule {
-    object_ownership = "BucketOwnerEnforced"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "anfw_flow_bucket_public_access_block" {
-  bucket = aws_s3_bucket.anfw_flow_bucket.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-resource "aws_networkfirewall_logging_configuration" "anfw_alert_logging_configuration" {
-  firewall_arn = module.network_firewall_dataexfiltration.arn
-  logging_configuration {
-    log_destination_config {
-      log_destination = {
-        logGroup = aws_cloudwatch_log_group.anfw_alert_log_group.name
-      }
-      log_destination_type = "CloudWatchLogs"
-      log_type             = "ALERT"
-    }
-    log_destination_config {
-      log_destination = {
-        bucketName = aws_s3_bucket.anfw_flow_bucket.bucket
-      }
-      log_destination_type = "S3"
-      log_type             = "FLOW"
-    }
-  }
-}
+# issue https://github.com/hashicorp/terraform-provider-aws/issues/38917
+# resource "aws_networkfirewall_logging_configuration" "network_firewall_logging_configuration" {
+#   firewall_arn = module.network_firewall_dataexfiltration.arn
+#   logging_configuration {
+#     log_destination_config {
+#       log_destination = {
+#         logGroup = aws_cloudwatch_log_group.network_firewall_log_group.name
+#       }
+#       log_destination_type = "CloudWatchLogs"
+#       log_type             = "ALERT"
+#     }
+#     log_destination_config {
+#       log_destination = {
+#         logGroup = aws_cloudwatch_log_group.network_firewall_log_group.name
+#       }
+#       log_destination_type = "CloudWatchLogs"
+#       log_type             = "FLOW"
+#     }
+#     log_destination_config {
+#       log_destination = {
+#         logGroup = aws_cloudwatch_log_group.network_firewall_log_group.name
+#       }
+#       log_destination_type = "CloudWatchLogs"
+#       log_type             = "TLS"
+#     }
+#   }
+# }
