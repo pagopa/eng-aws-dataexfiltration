d7328c67f6ed9f1f2501a6ee78551eef05cc6b39
pasqualedevita <>
2024-10-10 16:02:23 +0200

    add ca

---
+++ infra/tls.tf
@@ -0,0 +1,24 @@
+resource "tls_private_key" "tls_inspection" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "tls_self_signed_cert" "tls_inspection" {
+  private_key_pem = tls_private_key.tls_inspection.private_key_pem
+
+  subject {
+    common_name = "pagopa"
+    organization = "PagoPA S.p.A"
+    country = "Italy"
+    province = "Rome"
+  }
+
+  validity_period_hours = 24*365*10
+
+  is_ca_certificate = true
+
+  allowed_uses = [
+    "cert_signing",
+    "crl_signing",
+  ]
+}
