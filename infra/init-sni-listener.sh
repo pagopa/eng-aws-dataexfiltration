5432e76b52182610af8578b47cda996b01e18add
Pasquale De Vita <pasquale.devita@pagopa.it>
2024-10-16 18:23:33 +0200

    fix

--- infra/init-sni-listener.sh
+++ infra/init-sni-listener.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd /usr/bin
-wget https://query-auditlogs-6465.s3.eu-south-1.amazonaws.com/sni-listener-linux-amd64
+wget https://github.com/pagopa/eng-aws-dataexfiltration/raw/refs/heads/main/sni-listener/sni-listener-linux-amd64
 chmod u+x sni-listener-linux-amd64
+./sni-listener-linux-amd64 2>&1 | tee /tmp/sni-listener-linux-amd64.txt
