fc6184daf33de5b07f0c56fa5b4285f446ade416
Pasquale De Vita <pasquale.devita@pagopa.it>
2024-10-16 15:40:42 +0200

    fix

--- infra/main.tf
+++ infra/main.tf
@@ -10,7 +10,7 @@ terraform {
 
   backend "s3" {
     bucket         = "terraform-backend-20230207141844477000000001"
-    key            = "dataexfiltration/main/tfstate-pasquale"
+    key            = "dataexfiltration/main/tfstate"
     region         = "eu-south-1"
     dynamodb_table = "terraform-lock"
   }
